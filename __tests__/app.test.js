9c951f9114ecea0a44e52114d4a46ea995f6808a
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-04-02 19:11:40 -0700

    add post and get

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,34 @@
-const pool = require('../lib/utils/pool');
-const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+require('../lib/data/data-helpers');
 
-describe('TA-vanilla-js-fe-todos routes', () => {
-  beforeEach(() => {
-    return setup(pool);
+describe('TA-fullstack-vanillajs tests', () => {
+  it('should add a word and definition to the database', () => {
+    return request(app)
+      .post('/api/v1/words')
+      .send({
+        word: 'New word',
+        word_definition: 'New Word Definition'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          word: 'New word',
+          word_definition: 'New Word Definition'
+        });
+      });
+  });
+
+  it('should get all the words from the database', () => {
+    return request(app)
+      .get('/api/v1/words')
+      .then(res => {
+        expect(res.body).toEqual(expect.arrayContaining([
+          { id: expect.any(String), word: 'word 0', word_definition: 'word def 0' },
+          { id: expect.any(String), word: 'word 1', word_definition: 'word def 1' },
+          { id: expect.any(String), word: 'word 2', word_definition: 'word def 2' }
+        ])
+        );
+      });
   });
 });
