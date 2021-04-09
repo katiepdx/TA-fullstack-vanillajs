b08c8db0950810d57d77595998bed92f01002a78
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-04-08 18:04:58 -0700

    add jest mock for post route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,37 @@
 const request = require('supertest');
 const app = require('../lib/app');
-require('../lib/data/data-helpers');
+const { getDefinition } = require('../lib/utils/api-fetch');
+// require('../lib/data/data-helpers');
+
+// jest mocks a fetch
+const fetch = require('node-fetch');
+jest.mock('node-fetch');
 
 describe('TA-fullstack-vanillajs tests', () => {
-  it('should add a word and definition to the database', () => {
+  it.only('should add a word and definition to the database', () => {
+    
+    // mock from api
+    fetch.mockResolvedValue(
+      {
+        // returns the api res
+        async json() {
+          return {
+            definitions: [
+              {
+                type: 'NOUN',
+                definition: 'a road vehicle, typically with four wheels, powered by an internal-combustion engine and able to carry a small number of people.',
+                example: "we're going <b>by car</b>",
+                image_url: null,
+                emoji: null
+              }
+            ],
+            word: 'car',
+            pronunciation: 'kär'
+          };
+        }
+      }
+    );
+
     return request(app)
       .post('/api/v1/words')
       .send({
@@ -15,7 +43,7 @@ describe('TA-fullstack-vanillajs tests', () => {
           word: 'car',
           word_definition: 'a road vehicle, typically with four wheels, powered by an internal-combustion engine and able to carry a small number of people.'
         });
-      })
+      });
   });
 
   it('should get all the words from the database', () => {
